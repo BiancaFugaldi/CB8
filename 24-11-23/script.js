f4e2d5e36bbc5484568e76591e5e5dbdb494ab1c
BiancaFugaldi <148345178+BiancaFugaldi@users.noreply.github.com>
2023-11-26 21:09:59 +0100

    Add files via upload

---
+++ 24-11-23/script.js
@@ -0,0 +1,121 @@
+// NOT COMPLETED
+
+// const navWrapperEl = document.querySelector(".nav-wrapper");
+
+// const divLogo = () => {
+//   const divEl = document.createElement("div");
+//   divEl.className = "logo";
+//   const imgEl = document.createElement("img");
+//   imgEl.setAttribute("src", "./Media/logoipsum-288.svg");
+//   imgEl.setAttribute("alt", "logo");
+//   imgEl.setAttribute("width", "150px");
+//   imgEl.setAttribute("height", "100px");
+
+//   divEl.append(imgEl);
+//   navWrapperEl.append(divEl);
+// };
+
+// const divMenu = () => {
+//   const divEl = document.createElement("div");
+//   divEl.className = "menu";
+//   const ulEl = document.createElement("ul");
+//   const liEl1 = document.createElement("li");
+//   liEl1.textContent = "Home";
+//   const liEl2 = document.createElement("li");
+//   liEl2.textContent = "About";
+//   const liEl3 = document.createElement("li");
+//   liEl3.textContent = "Shop";
+//   const liEl4 = document.createElement("li");
+//   liEl4.textContent = "Pages";
+//   const liEl5 = document.createElement("li");
+//   liEl5.textContent = "Contacts";
+
+//   ulEl.append(liEl1, liEl2, liEl3, liEl4, liEl5);
+//   divEl.append(ulEl);
+//   navWrapperEl.append(divEl);
+// };
+
+// const divServiceMenu = () => {
+//   const divEl = document.createElement("div");
+//   divEl.className = "service-menu";
+//   const ulEl = document.createElement("ul");
+//   const liEl1 = document.createElement("li");
+//   liEl1.textContent = "Sign-up";
+//   const liEl2 = document.createElement("li");
+//   const buttonEl = document.createElement("button");
+//   buttonEl.className = "service-menu-btn";
+//   buttonEl.setAttribute("href", "./Pages/homepage.html");
+//   buttonEl.textContent = "Order now";
+
+//   liEl2.append(buttonEl);
+//   ulEl.append(liEl1, liEl2);
+//   divEl.append(ulEl);
+//   navWrapperEl.append(divEl);
+// };
+
+// const heroWrapperEl = document.querySelector(".hero-wrapper");
+
+// const divHeroText = () => {
+//   const divEl = document.createElement("div");
+//   divEl.className = "hero-text";
+//   const h1El = document.createElement("h1");
+//   h1El.textContent = "Lorem ipsum";
+//   const paragraphEl = document.createElement("p");
+//   paragraphEl.textContent =
+//     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio totam quibusdam cupiditate accusamus amet quis?";
+//   const buttonEl = document.createElement("button");
+//   buttonEl.textContent = "Get started";
+
+//   divEl.append(h1El, paragraphEl, buttonEl);
+//   heroWrapperEl.append(divEl);
+// };
+
+// const divHeroMedia = () => {
+//   const divEl = document.createElement("div");
+//   divEl.className = "hero-media";
+//   const imgEl = document.createElement("img");
+//   imgEl.setAttribute("src", "./Media/hero-img.png");
+//   imgEl.setAttribute("alt", "Trip");
+
+//   divEl.append(imgEl);
+//   heroWrapperEl.append(divEl);
+// };
+
+// const cardWrapperEl = document.querySelector(".card-wrapper");
+
+// const divCardIntro = () => {
+//   const divEl = document.createElement("div");
+//   divEl.className = "card-intro";
+//   const h3El = document.createElement("h3");
+//   h3El.textContent = "Lorem ipsum";
+//   const paragraphEl = document.createElement("p");
+//   paragraphEl.textContent =
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,  dolore.";
+
+//   divEl.append(h3El, paragraphEl);
+//   cardWrapperEl.append(divEl);
+// };
+
+// const divCards = () => {
+//   const divWrapper = document.createElement("div");
+//   divWrapper.className = "cards";
+//   const divEl = document.createElement("div");
+//   divEl.className = "first-card";
+//   const imgEl = document.createElement("img");
+//   (imgEl.className = "card-first-media"),
+//     imgEl.setAttribute("src", "./Media/card-mobile.png");
+//   imgEl.setAttribute("alt", "smartphone");
+//   const h6El = document.createElement("h6");
+//   h6El.textContent = "Lorem ipsum";
+//   const paragraphEl = document.createElement("p");
+//   paragraphEl.textContent =
+//     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, cumque?";
+
+// };
+
+// divLogo();
+// divMenu();
+// divServiceMenu();
+// divHeroText();
+// divHeroMedia();
+// divCardIntro();
